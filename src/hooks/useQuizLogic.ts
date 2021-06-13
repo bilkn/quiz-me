69b291c25c9ac5e3d738e1acd8fb60db79210090
bilkn <bilkan.konus@hotmail.com>
2021-06-13 19:18:25 +0300

    feat: add fetchQuizQuestions

--- src/hooks/useQuizLogic.ts
+++ src/hooks/useQuizLogic.ts
@@ -1,19 +1,50 @@
-import React, { useState } from 'react';
+import { CancelTokenSource } from 'axios';
+import React, { useEffect, useState } from 'react';
+import { fetchQuizQuestions, Difficulty } from '../API';
 
 function useQuizLogic() {
+  console.log();
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [number, setNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
-  const [score,setScore] = useState(0);
+  const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  useEffect(() => {
+    let cancelTokenObj: null | CancelTokenSource;
+    const fetchData = async () => {
+      const { data, cancelToken } = await fetchQuizQuestions(
+        10,
+        Difficulty.MEDIUM
+      );
+      console.log(data)
+      cancelTokenObj = cancelToken;
+      setQuestions(data);
+    };
+    try {
+      fetchData();
+    } catch (err) {
+      console.log(err);
+    }
+    return () => {
+      cancelTokenObj?.cancel();
+    };
+  }, []);
 
   const startTrivia = async () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
   const nextQuestion = () => {};
-  return { startTrivia, checkAnswer, nextQuestion,number,userAnswers,questions };
+  return {
+    startTrivia,
+    checkAnswer,
+    nextQuestion,
+    number,
+    userAnswers,
+    questions,
+  };
 }
 
 export default useQuizLogic;
